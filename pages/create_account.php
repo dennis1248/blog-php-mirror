f7bf886bd41871c304825714dcedba9dffeaa867
Dennis ten Hoove <dennistenhoove@protonmail.com>
2020-04-13 02:19:18 +0200

    SIGN UP fixes

--- pages/create_account.php
+++ pages/create_account.php
@@ -2,14 +2,14 @@
 
   <div class="login_box">
     <div class="login_box_header">
-      <h1 class="sign_in_header">Sign in</h1>
+      <h1 class="sign_in_header">Sign up</h1>
     </div>
     <div class="form_container">
       <form class="" action="/php/create_account.php" method="post">
         <input type="text" name="name" placeholder="Name">
         <input type="text" name="email" placeholder="Email Address">
         <input type="text" name="password" placeholder="Password">
-        <input type="button" name="create_acc_send" value="Create account">
+        <input type="button" name="create_acc_send" value="Sign me up">
       </form>
       <a class="form_footer" href="/pages/login.php">Already have an account?</a>
     </div>
