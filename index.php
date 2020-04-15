113d49f0a1387ec1b9836f880ed9e242f2905f93
Dennis ten Hoove <dennistenhoove@protonmail.com>
2020-04-15 14:42:44 +0000

    Revert "Add image to post"
    This reverts commit b630895ba44f665b8b711915ee0efc93ee1a061d

--- index.php
+++ index.php
@@ -13,35 +13,45 @@
           </div>
         </div>
       </div>
-      <?php for ($i = 1; $i <= 3; $i = $i + 2) { ?>
       <div class="container_box">
+        <?php if (true) : ?>
         <div class="paragraph boxed post">
           <div class="header_title">
             <h3>This is a post</h3>
           </div>
           <div class="article">
+            <p>Here the latest post will be written using PHP.</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
         </div>
+      <?php endif; ?>
       </div>
-      <?php } ?>
     </div>
     <div class="container">
-      <?php for ($i = 0; $i <= 2; $i = $i + 2) { ?>
       <div class="container_box">
         <div class="paragraph boxed post">
           <div class="header_title">
             <h3>This is a post</h3>
-          </div>
-          <div class="article_image" style="background:url(/images/cat.jpg);">
-
           </div>
           <div class="article">
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
           </div>
         </div>
       </div>
-    <?php } ?>
+      <div class="container_box">
+        <?php if (true) : ?>
+        <div class="paragraph boxed post">
+          <div class="header_title">
+            <h3>This is a post</h3>
+          </div>
+          <div class="article">
+            <p>Here the latest post will be written using PHP.</p>
+            <p>Note that the posts are different in height.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </div>
+      <?php endif; ?>
+      </div>
     </div>
   </div>
 
