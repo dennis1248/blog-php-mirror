781d9c10316f1cce43118c7f01658e2282c769d4
Dennis ten Hoove <dennistenhoove@protonmail.com>
2020-05-08 04:16:59 +0200

    Replace temp CSS order fix with SQL

--- functions/getposts.func.php
+++ functions/getposts.func.php
@@ -1,7 +1,7 @@
 <?php
   include ($_SERVER['DOCUMENT_ROOT'].'/functions/database.inc.php');
 
-  $sql = "SELECT * FROM  posts;";
+  $sql = "SELECT * FROM  posts ORDER BY id DESC;";
   $result = mysqli_query($conn, $sql);
   $resultCheck = mysqli_num_rows($result);
 
