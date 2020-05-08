5beeaac710009afc1211df9082bf08e4c0a1b2f1
Dennis ten Hoove <dennistenhoove@protonmail.com>
2020-05-08 05:06:43 +0200

    Mention git on projects

--- pages/projects.php
+++ pages/projects.php
@@ -1,4 +1,11 @@
 <?php include ($_SERVER['DOCUMENT_ROOT'].'/partials/header.php'); ?>
 
+  <div class="content">
+    <div class="container">
+      <div class="git_info">
+        <p>You can find the code for all of my projects on <a class="git_links" href="https://gitlab.com/dennis1248" target="_blank">Gitlab</a> and mirrors are available on <a class="git_links" href="https://github.com/dennis1248" target="_blank" >Github</a> </p>
+      </div>
+    </div>
+  </div>
 
 <?php include ($_SERVER['DOCUMENT_ROOT'].'/partials/footer.php'); ?>
