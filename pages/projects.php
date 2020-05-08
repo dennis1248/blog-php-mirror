6d654737dd6ad3a8d272782c16e576c1d26334df
Dennis ten Hoove <dennistenhoove@protonmail.com>
2020-05-08 05:09:56 +0200

    Move message content to default.conf for easy configuration

--- pages/projects.php
+++ pages/projects.php
@@ -3,7 +3,7 @@
   <div class="content">
     <div class="container">
       <div class="git_info">
-        <p>You can find the code for all of my projects on <a class="git_links" href="https://gitlab.com/dennis1248" target="_blank">Gitlab</a> and mirrors are available on <a class="git_links" href="https://github.com/dennis1248" target="_blank" >Github</a> </p>
+        <p> <?php echo $projectsMessage; ?> </p>
       </div>
     </div>
   </div>
