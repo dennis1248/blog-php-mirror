87108c2717ec1ec4a0609759c5883ae088ce043b
dennis1248 <dennistenhoove@protonmail.com>
2020-07-31 19:13:03 +0200

    Check if user owns post

--- functions/update.func.php
+++ functions/update.func.php
@@ -24,6 +24,31 @@
     exit();
   }
 
+  // Check if owner
+  if ($target === 'posts') {
+    $sql = 'SELECT owner FROM posts WHERE id=?;';
+  } elseif ($target === 'projects') {
+    $sql = 'SELECT owner FROM projects WHERE id=?;';
+  }
+  $stmt = mysqli_stmt_init($conn);
+
+  if (!mysqli_stmt_prepare($stmt, $sql)) {
+    header("Location: http://".$_SERVER['HTTP_HOST']."/?message=SQL statement failedddd&message-type=warning");
+    exit();
+  } else {
+    mysqli_stmt_bind_param($stmt, "s", $id);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    $row = mysqli_fetch_assoc($result);
+  }
+
+  mysqli_stmt_close($stmt);
+
+  if ($row['owner'] !== $_SESSION['userName']) {
+    header("Location: http://".$_SERVER['HTTP_HOST']."/?message=You do not own this content and are not allowed to edit it&message-type=warning");
+    exit();
+  }
+
   // Check if id is valid
   if (!is_numeric($id)) {
     header("Location: http://".$_SERVER['HTTP_HOST']."/?message=Invalid post datatype&message-type=warning");
